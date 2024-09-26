2352272848bb2a256b79416011e4362659c819e1
vitoriasalim <148975005+vitoriasalim@users.noreply.github.com>
2024-09-26 14:49:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+const perguntas = [
+    {
+        pergunta: 'Qual é a raça de cachorro mais popular no mundo?',
+        resposta: 'O Labrador Retriever é a raça de cachorro mais popular.'
+    },
+    {
+        pergunta: 'Quantos anos, em média, vive um cachorro?',
+        resposta: 'A expectativa de vida de um cachorro varia entre 10 e 13 anos, dependendo da raça.'
+    },
+    {
+        pergunta: 'Como os cães se comunicam?',
+        resposta: 'Os cães se comunicam através de latidos, rosnados, postura corporal e até mesmo lambidas.'
+    },
+    {
+        pergunta: 'Por que os cachorros abanam o rabo?',
+        resposta: 'Os cachorros abanam o rabo para demonstrar felicidade ou excitação.'
+    },
+    {
+        pergunta: 'Qual é a menor raça de cachorro?',
+        resposta: 'O Chihuahua é considerado a menor raça de cachorro.'
+    },
+    {
+        pergunta: 'Os cães podem entender palavras humanas?',
+        resposta: 'Sim, estudos mostram que os cães podem entender até 165 palavras diferentes.'
+    },
+    {
+        pergunta: 'Por que os cães cheiram outros cães?',
+        resposta: 'Os cães cheiram outros cães para obter informações sobre o estado emocional, saúde e até dieta do outro cão.'
+    },
+    {
+        pergunta: 'Como os cães ajudam os humanos?',
+        resposta: 'Os cães ajudam os humanos como cães de serviço, farejadores, terapeutas e até cães de resgate.'
+    },
+    {
+        pergunta: 'Os cães conseguem sonhar?',
+        resposta: 'Sim, estudos indicam que os cães sonham, provavelmente com atividades que realizam durante o dia.'
+    },
+    {
+        pergunta: 'Por que os cães gostam de cavar?',
+        resposta: 'Cavar pode ser um instinto natural de cães para criar um abrigo ou esconder alimentos.'
+    }
+];
